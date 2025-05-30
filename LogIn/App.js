edd4da08a90c3be4071eae6e6015af6bf4577f6c
BRUNO NAHUEL MARASI <48113202@ort>
2025-05-30 10:37:16 -0300

    casiListo

--- LogIn/App.js
+++ LogIn/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import Entypo from '@expo/vector-icons/Entypo';
@@ -98,7 +98,9 @@ export default function App() {
       <StatusBar barStyle="light-content" />
       <NavigationContainer>
           {!isLoggedIn ? (
-            <LogIn setIsLoggedIn={setIsLoggedIn}/>
+            <LogIn setIsLoggedIn={setIsLoggedIn}
+              styles={styles}
+            />
           ) : (
             <MyTabs />
           )}
@@ -106,3 +108,58 @@ export default function App() {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    marginBottom: 100,
+    width: '45%',
+    height: '20%',
+    borderRadius: 25,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#1359BF',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 15,
+    width: '90%',
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    color: '#000',
+    fontSize: 16,
+  },
+  loginButton: {
+    backgroundColor: '#29426B',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width: '90%',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  loginText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  forgotPassword: {
+    marginTop: 15,
+    color: '#3C49D2',
+    fontSize: 13,
+  },
+});
--- LogIn/screens/logIn.js
+++ LogIn/screens/logIn.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { StatusBar, SafeAreaView, Image, View, TextInput, Pressable, Text, StyleSheet } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 
-const LogIn = ({ setIsLoggedIn }) => {
+const LogIn = ({ setIsLoggedIn, styles }) => {
   const [company, setCompany] = useState('');
   const [password, setPassword] = useState('');
 
@@ -46,60 +46,5 @@ const LogIn = ({ setIsLoggedIn }) => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-  },
-  logo: {
-    marginBottom: 100,
-    width: '45%',
-    height: '20%',
-    borderRadius: 25,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#1359BF',
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    marginBottom: 15,
-    width: '90%',
-  },
-  icon: {
-    marginRight: 10,
-  },
-  input: {
-    flex: 1,
-    color: '#000',
-    fontSize: 16,
-  },
-  loginButton: {
-    backgroundColor: '#29426B',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    width: '90%',
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  loginText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  forgotPassword: {
-    marginTop: 15,
-    color: '#3C49D2',
-    fontSize: 13,
-  },
-});
 
 export default LogIn;
